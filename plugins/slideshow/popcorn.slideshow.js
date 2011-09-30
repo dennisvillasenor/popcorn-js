668373670c8ed590e1ee5bb8a9a76676ddb4e259
unknown <wolfleader@.(none)>
2011-09-29 20:40:20 -0400

    updated the text for slideshow plugin

--- plugins/slideshow/popcorn.slideshow.js
+++ plugins/slideshow/popcorn.slideshow.js
@@ -1,4 +1,16 @@
 /* 
 * Popcorn Slideshow Plugin
-
+* Much of the details of this plugin are still being ironed out with Dave 
+* Seifried basically this plugin will be a slideshow application that will run 
+* under Baseplayer.   The slideshow will have a manual or auto play mode in
+* which the slideshow will move through each slide either after user input such
+* as a keystroke (manual) or after a period of time has passed (auto).
+*
+* As already mentioned Baseplayer will be used to run the application though how
+* it will do so I will still need clarification from Dave and Scott.
+*
+* This slideshow will work in a similar fashion to a powerpoint presentation
+* than a conventional image slideshow allowing users to show text, images and
+* quite possibly even embedded video without the need for installing a flash
+* plugin.
 */
